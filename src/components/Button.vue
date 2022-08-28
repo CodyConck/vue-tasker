f38e9b0fffdbbbbcc79a8254a4821d8a5d8d3960
CodyConck <codyconck@gmail.com>
2022-08-28 16:26:15 -0500

    began tut on vue task tracking app

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- for event you can use v-on:click tooor abbrev like below with @ -->
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      console.log("clicked");
+    },
+  },
+};
+</script>
